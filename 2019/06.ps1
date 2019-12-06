0344cd304cd72e0429dfb9eba2fe64396961c806
Eirik Haver <eiriha@gmail.com>
2019-12-06 09:51:34 +0100

    Day 6 was hackish

---
+++ 2019/06.ps1
@@ -0,0 +1,80 @@
+﻿# Exceptionally Hacky Solution!
+
+$data = Get-Content -Path (Join-Path $PSScriptRoot "06-input.txt") 
+$data2 = $data| ForEach-Object {
+    [PSCustomObject][ordered]@{
+        "inner" = $_ -split "\)" | Select -first 1
+        "outer" = $_ -split "\)" | Select -last 1   
+    }
+
+}
+
+$AllNodes = @{}
+$data2 | ForEAch-Object {
+    $AllNodes[$_.Outer] = $_.Inner
+    
+}
+
+$CounTotal = 0
+
+$AllNodes.Keys | ForEach-Object {
+    $current = $AllNodes[$_]
+    $currentCount = 1
+    while($AllNodes.ContainsKey($current)) {
+        $currentCount += 1
+        $current = $AllNodes[$current]
+    }
+    $CounTotal += $currentCount
+}
+
+Write-Warning ("Task 1:{0}" -f $CounTotal)
+
+$YouPath = @()
+$SanPAth = @()
+
+$current = $AllNodes["YOU"]
+while($AllNodes.ContainsKey($current)) {
+    $YouPath += $current
+    $current = $AllNodes[$current]
+
+}
+
+$SanPAth = @()
+$current = $AllNodes["SAN"]
+while($AllNodes.ContainsKey($current)) {
+    $SanPath += $current
+    $current = $AllNodes[$current]
+
+}
+
+$CommonNodes = $YouPath | Where-Object { $_ -in $SanPAth }
+
+$ShortestPath = 9999
+
+$CommonNodes | ForEach-Object {
+    $commonNode = $_
+    $YouDistance = 0
+    $SanDistance = 0
+    $TotalDistance = 0
+    for ($i = 1; $i -lt $YouPath.Count; $i++)
+    { 
+        if($YouPath[$i] -eq $CommonNode) {
+            $YouDistance = $i
+            break;
+        }
+    }
+    for ($i = 1; $i -lt $SanPath.Count; $i++)
+    { 
+        if($SanPath[$i] -eq $CommonNode) {
+            $SanDistance = $i
+            break;
+        }
+    }
+    $TotalDistance = $SanDistance + $YouDistance
+
+    if($TotalDistance -lt $ShortestPath) {
+        $ShortestPath = $TotalDistance
+    }
+}
+
+Write-Warning ("Task 2:{0}" -f $ShortestPath)
